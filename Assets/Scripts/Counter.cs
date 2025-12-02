dcc0cec5ad24cc9426c407f855b4cc937725b01e
artemcurko7-art <artemcurko7@gmail.com>
2025-12-02 21:55:06 +0300

    Init

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -4,44 +4,64 @@ using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
+    [SerializeField] private InputReader _inputReader;
     [SerializeField] private float _delay;
 
     public event Action Changed;
 
-    private int _numberCounter;
-    private bool _isPressedButton;
     private float _elapsedTime;
+    private bool _isCounterOpen;
 
-    public int NumberCounter => _numberCounter;
+    public int NumberCounter { get; private set; }
 
-    private void Update()
+    private void OnEnable()
     {
-        OnMouseButton();
+        _inputReader.PressedButton += OnButtonClick;
     }
 
-    private void OnMouseButton()
+    private void OnDisable()
     {
-        if (Input.GetMouseButtonDown(0) && _isPressedButton)
-            _isPressedButton = false;
-        else if (Input.GetMouseButtonDown(0) && _isPressedButton == false)
-            _isPressedButton = true;
+        _inputReader.PressedButton -= OnButtonClick;
+    }
+
+    private void OnButtonClick()
+    {
+        if (_isCounterOpen == false)
+        {
+            Start();
+            return;
+        }
+
+        Stop();
+    }
+
+    private void Start()
+    {
+        _isCounterOpen = true;
 
         StartCoroutine(ChangeNumberCounter());
     }
 
+    private void Stop()
+    {
+        _isCounterOpen = false;
+    }
+
     private IEnumerator ChangeNumberCounter()
     {
-        if (_isPressedButton == false)
-            yield break;
-
-        _elapsedTime += Time.deltaTime;
-
-        if (_elapsedTime >= _delay)
+        while (_isCounterOpen)
         {
-            _numberCounter++;
-            Changed?.Invoke();
+            _elapsedTime += Time.deltaTime;                    
 
-            _elapsedTime = 0;
+            if (_elapsedTime >= _delay)
+            {
+                NumberCounter++;
+                Changed?.Invoke();
+
+                _elapsedTime = 0;
+            }
+
+            yield return null;
         }
     }
 }
